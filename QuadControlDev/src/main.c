3105a987b17e0ac7f71aa89d329ecb7bb9a82ff5
User-PC\User <bcostandin@yahoo.com>
2017-10-09 23:39:30 +0300

    rcc update

--- QuadControlDev/src/main.c
+++ QuadControlDev/src/main.c
@@ -19,7 +19,25 @@
 
 int main(void)
 {
+	/*Init HAL library*/
+	HAL_Init();
 
+	/*Enable HSI oscillator and configure the PLL to reach the maximum frequency 180 MHz when using HSI oscillator as PLL clock source*/
+	SystemClockConfiguration();
+
+	/* Configure LED1,LED2,LED3 */
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+	BSP_LED_Init(LED3);
+
+	/* Initialize User push-button, will be used to trigger an interrupt each time it's pressed.*/
+	BSP_PB_Init(BUTTON_USER,BUTTON_MODE_EXTI);
+
+	/* Output SYSCLK  / 1 on MCO1 pin(PA.08) */
+	HAL_RCC_MCOnfig(RCC_MCO1,RCC_MCO1SOURCE_PLLCLK,RCC_MCODIV_1);
+
+
+	/*toggle LEDS*/
 	while(1)
 	{
 
